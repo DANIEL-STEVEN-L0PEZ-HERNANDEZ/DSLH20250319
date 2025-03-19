0753ea50b6eee0d909ea2ea82cab17abeaa12607
DANIEL STEVEN LÓPEZ HERNÁNDEZ <daniel2006503@outlook.com>
2025-03-19 10:22:55 -0300

    Se modificó el controlado de usuario el modelo de usuario y la vista Layout

--- DSLH20250319.AppWebMVC/Controllers/UserController.cs
+++ DSLH20250319.AppWebMVC/Controllers/UserController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DSLH20250319.AppWebMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace DSLH20250319.AppWebMVC.Controllers
 {
@@ -63,6 +69,116 @@ namespace DSLH20250319.AppWebMVC.Controllers
             }
             return View(user);
         }
+
+
+
+
+
+        [AllowAnonymous]
+        public async Task<IActionResult> CerrarSession()
+        {
+            // Hola mundo
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(User usuario)
+        {
+            usuario.PasswordHash = CalcularHashMD5(usuario.PasswordHash);
+            var usuarioAuth = await _context.
+                Users.
+                FirstOrDefaultAsync(s => s.Email == usuario.Email && s.PasswordHash == usuario.PasswordHash);
+            if (usuarioAuth != null && usuarioAuth.UserId > 0 && usuarioAuth.Email == usuario.Email)
+            {
+                var claims = new[] {
+                    new Claim(ClaimTypes.Name, usuarioAuth.Email),
+                    new Claim("Id", usuarioAuth.UserId.ToString()),
+                    new Claim("Nombre", usuarioAuth.Username),
+                    new Claim(ClaimTypes.Role, usuarioAuth.Role)
+                    };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "El email o contraseña estan incorrectos");
+                return View();
+            }
+        }
+
+        private string CalcularHashMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); 
+                }
+                return sb.ToString();
+            }
+        }
+
+
+
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Perfil(int id, [Bind("Id,Nombre,Email,Estatus,Rol")] User usuario)
+        {
+            if (id != usuario.UserId)
+            {
+                return NotFound();
+            }
+            var usuarioUpdate = await _context.Users
+                 .FirstOrDefaultAsync(m => m.UserId == usuario.UserId);
+            try
+            {
+                usuarioUpdate.Username = usuario.Username;
+                usuarioUpdate.Email = usuario.Email;
+                usuarioUpdate.Estatus = usuario.Estatus;
+                usuarioUpdate.Role= usuario.Role;
+                _context.Update(usuarioUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(usuario.UserId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(usuario);
+                }
+            }
+        }
+
+
+
+
+
+
+
 
         // GET: User/Edit/5
         public async Task<IActionResult> Edit(int? id)
--- DSLH20250319.AppWebMVC/Models/User.cs
+++ DSLH20250319.AppWebMVC/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DSLH20250319.AppWebMVC.Models;
 
@@ -7,11 +9,40 @@ public partial class User
 {
     public int UserId { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Username { get; set; } = null!;
 
+    [Required(ErrorMessage = "El Email es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
+    [Required(ErrorMessage = "El password es obligatorio.")]
+    [DataType(DataType.Password)]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El passowrd debe tener entre 5 y 50 caracteres.")]
     public string PasswordHash { get; set; } = null!;
 
     public string Role { get; set; } = null!;
+
+    public byte? Estatus { get; set; } = 1;
+
+    private string strEstatus;
+    [NotMapped]
+    public string StrEstatus
+    {
+        get
+        {
+            if (Estatus == 1)
+                strEstatus = "Activo";
+            else if (Estatus == 2)
+                strEstatus = "Inactivo";
+            return strEstatus;
+        }
+        set { strEstatus = value; }
+    }
+    [NotMapped]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El password debe tener entre 5 y 50 caracteres.")]
+    [Display(Name = "Confirmar Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+    public string? ConfirmarPassword { get; set; } = null!;
 }
