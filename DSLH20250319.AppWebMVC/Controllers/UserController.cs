7733762f03b3cb60f3c9dabe0437a5f7a9bd117d
DANIEL STEVEN LÓPEZ HERNÁNDEZ <daniel2006503@outlook.com>
2025-03-19 11:33:22 -0300

    Se modifico el controlador usuario el layout y la vista de perfil

--- DSLH20250319.AppWebMVC/Controllers/UserController.cs
+++ DSLH20250319.AppWebMVC/Controllers/UserController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -166,11 +163,6 @@ namespace DSLH20250319.AppWebMVC.Controllers
                 }
             }
         }
-
-
-
-
-
 
 
 
